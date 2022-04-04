6f29e20db7beb363e988593301274746246dc371
ThienHoanNeee <softteam2021@gmail.com>
2022-04-04 15:03:31 +0700

    [Init] init project with spring boot

---
+++ src/main/java/com/example/AccomadationForStudent/AccomadationForStudentApplication.java
@@ -0,0 +1,13 @@
+package com.example.AccomadationForStudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccomadationForStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccomadationForStudentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AccomadationForStudent/AccomadationForStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccomadationForStudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccomadationForStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
